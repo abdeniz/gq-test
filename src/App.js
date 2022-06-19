9a68ee70b332fff5c218bc835b455650acbf6af9
Deniz Abdurrahmani <abdurrahmani.deniz00@gmail.com>
2022-06-19 16:35:22 +0300

    Supabase testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import {ChakraProvider} from "@chakra-ui/react";
-import styled from "styled-components";
-import "./App.css";
-import Graph from "./Graph";
+import "./index.css";
+import {useState, useEffect} from "react";
+import {supabase} from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+import {Button} from "@chakra-ui/react";
 
-function App() {
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  async function signInWithGithub() {
+    const {user, session, error} = await supabase.auth.signIn({
+      provider: "github",
+    });
+
+    console.log(session);
+
+    setSession(session);
+  }
+
   return (
-    <ChakraProvider>
-      <div className="wrapper">
-        <Wrapper>
-          <Graph />
-        </Wrapper>
-      </div>
-    </ChakraProvider>
+    <div className="container" style={{padding: "50px 0 100px 0"}}>
+      {!session ? (
+        <Button onClick={() => signInWithGithub()}>Log in Github</Button>
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
   );
 }
-
-const Wrapper = styled.div`
-  position: relative;
-`;
-
-export default App;
